9e13c3be641eb9bc8dc30e036ce3d07ae3080de1
FirstAmendment <oscaralexisaguirre@gmail.com>
2023-10-22 21:09:19 -0700

    10/22/23
    Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Track location on a 3D plane 
+// Author: Oscar Aguirre
+// Date: 10/23/2023
 
 // Constants
 
@@ -19,3 +19,50 @@ function main() {
 
 // let's get this party started
 main();
+/**
+Pseudocode - Everyday task 
+Freaking a Black and Mild
+ 1) Removing tobacco from B&M
+Remove the wrapper ring 
+Remove smaller wrapper bit
+Remove larger wrapper bit
+Roll the B&M between fingers 
+While you can feel tobacco inside the B&M ;  Roll B&M between fingers until the tobacco falls out 
+If the inner wrapper is not glued properly ; then remove it with pliers 
+Else ; leave the inner wrapper glued in the B&M
+2) Breaking down the weed
+Remove weed from bag 
+Break raisin sized nuggets off of the stem
+Loop until 1.5 grams has been broken down
+3)  Filling the Black and Mild 
+Drop the raisin sized weed nuggets inside the empty B&M
+Lightly pack with a packer
+loop the last two steps until the B&M is full 
+
+Pseudocode - Track Your Location 
+1 ) Initialize Program 
+  X = 0
+  Y = 0
+  Z = 0
+2) Initial Input  
+  Variable Input; 
+  User Input = Input
+    Print “Input : Forward, Backward, Left, Right, Up, Down?”
+      If Input = Forward : Then X + 1, Initiate Secondary Input
+      If Input = Backward : Then X - 1, Initiate Secondary Input
+      If Input = Left : Then Y + 1, Initiate Secondary Input
+      If Input = Right : Then Y - 1, Initiate Secondary Input
+      If Input = Up : Then Z + 1, Initiate Secondary Input
+      If Input = Down : Then Z - 1, Initiate Secondary Input
+      Else = Print “Not a valid input” + loop Initial Input 
+3) Secondary Input  
+  Variable Continue;
+      Print “Your Location : “
+        Print “ X = “ , Print X
+        Print “ Y = “ , Print Y
+        Print “ Z = “ , Print Z
+  Print “Continue? Yes , No”
+      If Continue = Yes ; Then initiate initial input
+      If Continue = No : Then End program
+      Else  = Print “Not a valid input” + loop Secondary Input
+ */
