4a0bc0e858aa269a59b0d1c895fbfe26e3e0063b
FirstAmendment <oscaralexisaguirre@gmail.com>
2023-10-25 08:07:57 -0700

    10-25-23
    updating js

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -13,7 +13,9 @@ ownIt = false;
 //Calculate Age
 //user10060795 on stack overflow provided this code snippet for retreiving the current year from my local machines time
 //https://stackoverflow.com/questions/6002254/get-the-current-year-in-javascript#:~:text=Instantiate%20the%20class%20Date%20and,new%20Date().getFullYear()%3B 
-now = new Date().getFullYear();
+//now = new Date().getFullYear();
+//age = now - year;
+now = 2023;
 age = now - year;
 //output
 document.writeln("<br>make : " + make + "<br>");
